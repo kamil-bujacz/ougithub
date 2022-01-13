2b84f4ca41ec5aee2b2624b3f96a1a733245960f
Kamil Bujacz <kiklas@MacBook-Air-Kamil.local>
2022-01-13 16:35:35 +0100

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/ougithub.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ougithub.dir/main.cpp.o"
+  "ougithub"
+  "ougithub.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ougithub.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/ougithub.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for ougithub.
+# This may be replaced when dependencies are built.
